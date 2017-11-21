2170962bc6d2a83f4b762b3f1de3de72e0245401
annalanigan <annalanigan@outlook.com>
2017-11-21 20:25:19 +0000

    making changes with CSS

--- controller.rb
+++ controller.rb
@@ -3,10 +3,18 @@ require 'sinatra/reloader'
 require_relative 'models/game'
 
 get '/' do
-  'Home'
+  "Home Screen"
 end
 
-get '/:hand1/:hand2' do
+get '/welcome' do
+  erb (:welcome)
+end
+
+get '/game' do
+ erb (:game)
+end
+
+get '/game/:hand1/:hand2' do
   hand1 = params[:hand1]
   hand2 = params[:hand2]
   game1 = Game.new(hand1, hand2)
